10507fd9ff83453225c95552edb5011a7d29ada0
Energysmile <myxtarok@mail.ru>
2024-08-01 19:27:53 +0300

    Complete Homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        System.out.println("Задание 1");
+        int [] costAmount= {17_000, 20_000, 16_000, 21_000, 20_500};
+        int sum = 0;
+        for (int i = 0; i < costAmount.length; i++) {
+            sum = sum + costAmount[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+        //Задание 2
+        System.out.println("Задание 2");
+        int minCostAmount = 1_000_000;
+        for (int i = 0; i < costAmount.length; i++) {
+            if (costAmount[i] < minCostAmount) {
+                minCostAmount = costAmount[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + minCostAmount + " рублей");
+        int maxCostAmount = 0;
+        for (int i = 0; i < costAmount.length; i++) {
+            if (costAmount[i] > maxCostAmount) {
+                maxCostAmount = costAmount[i];
+            }
+
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + maxCostAmount + " рублей");
+
+        //Задание 3
+        System.out.println("Задание 3");
+        double averageAmount = sum / (costAmount.length - 1);
+        System.out.println("Средняя сумма трат за месяц составила " + averageAmount + " рублей");
+
+        //Задание 4
+        System.out.println("Задание 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
